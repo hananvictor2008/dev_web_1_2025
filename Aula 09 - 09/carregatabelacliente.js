c008b49bb7fcb3369e2a2d21f66a2b898b2fbf58
Hanan <165320332+hananvictor2008@users.noreply.github.com>
2025-09-11 15:39:14 -0300

    Add files via upload

--- Aula 09 - 09/carregatabelacliente.js
+++ Aula 09 - 09/carregatabelacliente.js
@@ -1,37 +1,25 @@
-let clientes = {
-    lista: [
-        { nome: "José", id: 2, nascimento: "Bom Jardim" },
-        { nome: "Maria", id: 3, nascimento: "Recife" },
-        { nome: "Ana", id: 4, nascimento: "Olinda" },
-        { nome: "Carlos", id: 5, nascimento: "Caruaru" },
-        { nome: "Fernanda", id: 6, nascimento: "Garanhuns" },
-        { nome: "João", id: 7, nascimento: "Petrolina" },
-        { nome: "Luciana", id: 8, nascimento: "Paulista" },
-        { nome: "Marcos", id: 9, nascimento: "Jaboatão dos Guararapes" },
-        { nome: "Patrícia", id: 10, nascimento: "Camaragibe" },
-        { nome: "Ricardo", id: 11, nascimento: "Gravatá" },
-        { nome: "Sônia", id: 12, nascimento: "Vitória de Santo Antão" },
-        { nome: "Daniel", id: 13, nascimento: "Arcoverde" },
-        { nome: "Rafaela", id: 14, nascimento: "Serra Talhada" },
-        { nome: "Bruno", id: 15, nascimento: "Igarassu" },
-        { nome: "Larissa", id: 16, nascimento: "Palmares" }
-    ]
-}
+//deve iniciar o servidor com o json, ver a Aula 09 - 11
+const url = "http://localhost:3000/cliente"
 
 document.addEventListener("DOMContentLoaded", (evento)=>{
     evento.preventDefault();
-    let tabela = document.getElementsByTagName("table")[0]//retorna o primeiro table do código
-    clientes.lista.forEach(cliente =>{
-        const linha = document.createElement("tr")
-        const colunaNome = document.createElement("td")
-        const colunaNascimento = document.createElement("td")
+    const tabela = document.getElementsByTagName("table")[0]//retorna o primeiro table do código
 
-        colunaNome.textContent = cliente.nome
-        colunaNascimento.textContent = cliente.nascimento
+    fetch(url).then( async resposta => { //deve ser async para usar o await
+        const clientes = await resposta.json() //converte a resposta para JSON e aguarda a conversão, sem o await, retorna uma Promise, com o await, retorna o JSON
+        console.log(clientes)
+
+        clientes.forEach(cliente =>{
+            const linha = document.createElement("tr")
+            const colunaNome = document.createElement("td")
+            const colunaNascimento = document.createElement("td")
+
+            colunaNome.textContent = cliente.nome
+            colunaNascimento.textContent = cliente.nascimento
         
-        tabela.appendChild(linha)
-        linha.appendChild(colunaNome)
-        linha.appendChild(colunaNascimento)
-
+            tabela.appendChild(linha)
+            linha.appendChild(colunaNome)
+            linha.appendChild(colunaNascimento)
+        })
     })
 })
